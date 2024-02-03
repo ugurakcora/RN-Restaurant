1136f08354fa18140ead2374782ab9680a8879a8
ugurakcora <uakcora97@gmail.com>
2024-02-03 18:45:37 +0300

    Added Axios and Filter

---
+++ components/ResultsList.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ResultsList({ title, results }) {
+  console.log(results);
+  return (
+    <View>
+      <Text>ResultsList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,11 +1,34 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 export default function SearchScreen() {
+  const [searchApi, results] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
+  console.log(results);
   return (
     <View>
       <SearchBar />
+      <ResultsList
+        title="Ucuz Restoranlar"
+        results={filterResultsByPrice("₺")}
+      />
+      <ResultsList
+        title="Uygun Restoranlar"
+        results={filterResultsByPrice("₺₺")}
+      />
+      <ResultsList
+        title="Pahalı Restoranlar"
+        results={filterResultsByPrice("₺₺₺")}
+      />
     </View>
   );
 }
