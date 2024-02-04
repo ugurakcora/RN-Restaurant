d3f2c97c1510e321280853422ea5d461819a56c0
ugurakcora <uakcora97@gmail.com>
2024-02-05 00:23:14 +0300

    added env

--- api/yelp.js
+++ api/yelp.js
@@ -1,9 +1,9 @@
 import axios from "axios";
+import { API_URL, API_TOKEN } from "@env";
 
 export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
+  baseURL: `${API_URL}/businesses`,
   headers: {
-    Authorization:
-      "Bearer aETOPuVSQ-luGy16YjLMitcWYtZh4W9pRa8Z8hk5Tz42JZA2f2UPY0ve9r7XDLnrlfVrbVqqJ-MagxP1AugW2wZgFVw2cl2ci8tq0UnFZRzUsT7_x_0-7hVa4F-RZHYx",
+    Authorization: `Bearer ${API_TOKEN}`,
   },
 });
